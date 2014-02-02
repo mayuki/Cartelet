7f6bddbfee1928acd3be4e4642884954314d3d0e
Mayuki Sawatari <mayuki@misuzilla.org>
2014-02-03 01:57:06 +0900

    使い方がわからない気がするのでサンプルを追加。

---
+++ Cartelet.Sample.Web/App_Start/CarteletConfig.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using Cartelet.Html;
+using Cartelet.Mvc;
+
+namespace Cartelet.Sample.Web
+{
+    public class CarteletConfig
+    {
+        public static void Register(ViewEngineCollection viewEngineCollection)
+        {
+            // CarteletのHTMLフィルターを作成
+            var htmlFilter = new HtmlFilter();
+            
+            // HTMLフィルターにCSS(~/Asset/Css/feature.css)を展開するハンドラなどを登録
+            // 更新チェックはMvcApplication.Application_BeginRequestで
+            StylesheetExpander.StylesheetExpander.Register(htmlFilter, HttpContext.Current.Server.MapPath("~/Asset/Css/feature.css"));
+
+            // Carteletを通すViewEngineを登録
+            var viewEngine = new CarteletViewEngine(new RazorViewEngine());
+            viewEngineCollection.Clear();
+            viewEngineCollection.Add(viewEngine);
+
+            // Carteletのコンテキスト(変換の単位=リクエストごとのオブジェクト)を生成するメソッドをViewEngineにセット
+            viewEngine.CarteletContextFactory = (content, writer) =>
+                                                {
+                                                    // 例えばガラケーの時だけWriterをかぶせるとか…
+                                                    // ここでは小文字化するWriterをセットする
+                                                    var ctx = new CarteletContext(content, writer);
+                                                    ctx.Writer = new LowerTextWriter(writer, ctx);
+                                                    return ctx;
+                                                };
+
+            // HTMLフィルターを取得するためのメソッドをセット(ファクトリだけどスレッドセーフな処理な限り、同じものを返しても問題ない)
+            viewEngine.HtmlFilterFactory = () =>
+                                           {
+                                               return htmlFilter;
+                                           };
+
+
+            // その他フィルター
+            htmlFilter.AddHandler(".to-lower", (context, node) =>
+                                               {
+                                                   // 小文字化Writerが有効になるようにContextのItemsにtrueをセットする
+                                                   // このデータはマッチした要素より上の要素には影響を与えない
+                                                   context.Items.Set("Cartelet.Sample.Web.UpperTextWriter:Enable", true);
+                                                   return true;
+                                               });
+            htmlFilter.AddHandler("img", (context, node) =>
+                                                       {
+                                                           // 画像のalt属性がなかったらsrc属性から付けてみる
+                                                           if (!node.Attributes.ContainsKey("alt"))
+                                                           {
+                                                               node.Attributes["alt"] = node.Attributes["title"] = Path.GetFileName(node.Attributes["src"]);
+                                                           }
+                                                           return true;
+                                                       });
+        }
+    }
+
+    /// <summary>
+    /// 文字列をすべて小文字にするWriter
+    /// </summary>
+    public class LowerTextWriter : TextWriter
+    {
+        public TextWriter BaseWriter { get; private set; }
+        private CarteletContext _ctx;
+
+        public LowerTextWriter(TextWriter writer, CarteletContext ctx)
+        {
+            BaseWriter = writer;
+            _ctx = ctx;
+        }
+
+        public override void Write(char value)
+        {
+            if (_ctx.Items.Get<Boolean>("Cartelet.Sample.Web.UpperTextWriter:Enable"))
+            {
+                BaseWriter.Write(Char.ToLower(value));
+            }
+            else
+            {
+                BaseWriter.Write(value);
+            }
+        }
+
+        public override void Write(string value)
+        {
+            if (_ctx.Items.Get<Boolean>("Cartelet.Sample.Web.UpperTextWriter:Enable"))
+            {
+                BaseWriter.Write(value.ToLower());
+            }
+            else
+            {
+                BaseWriter.Write(value);
+            }
+        }
+
+        public override Encoding Encoding
+        {
+            get { return BaseWriter.Encoding; }
+        }
+    }
+}
---
+++ Cartelet.Sample.Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Cartelet.Sample.Web")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Cartelet.Sample.Web")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3ca98b69-0aa8-4d80-b0c7-4984a384803a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
